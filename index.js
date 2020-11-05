7f011c3ae9639668ae255d4d65d1c2c05bcc65d6
Stacydina <stacydina6@gmail.com>
2020-11-05 13:43:23 +0300

    Catalogue created

--- index.js
+++ index.js
@@ -4,31 +4,49 @@
         sku: index + 1,
         name: `pet ${index +1}`,
         amount: (Math.floor(Math.random() * 100)),
-        availability: true
+        presence: true,
       
      }));
   
   
   // Display Elements on the browser   
 var html = catalogue.map(function (element) {
-    return  '<li>' + element.sku + ', ' +
-      element.name+ ', ' + element.amount + (',')   +'</li>';  
+    
+      
+      let buttonCreate = () =>{
+        let button = document.createElement("button");
+        button.innerText = "Buy";
+        
+        var body = document.getElementsByTagName("body")[0];
+        body.appendChild(button);
+        button.onclick = e =>{
+          var clicking = document.getElementById('item');
+          
+          button.innerHTML = "available";
+          if (element.available == true){
+            
+            clicking.classList.add("success");
+            button.innerHTML = "available"
+          }
+          else{
+            clicking.classList.add("notAvailable");
+            button.innerHTML = "Not available"
+          }
+          
+        
+        }
+        };
+       
+    return  '<li>' + element.sku   +',' +
+      element.name+ ', ' + element.amount + (',') + element.presence + (',')  +'</li>';  
  
       } ).join('');
-     
+      
+   
   document.getElementById('item').innerHTML= html;
    
-let buttonCreate = () =>{
-let button = document.createElement("button");
-button.innerText = "Buy";
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-button.onclick = e =>{
 
-}
-};
-    
-
+       
 
     
 
